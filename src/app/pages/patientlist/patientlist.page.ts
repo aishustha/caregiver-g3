16463031b13c3d23bf58a63f96a927fa571b0d1d
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-03-06 23:28:33 -0500

    added pages

--- src/app/pages/patientlist/patientlist.page.ts
+++ src/app/pages/patientlist/patientlist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-patientlist',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientlistPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  navigateToPatientInfo() {
+    this.router.navigate(['home/patientinfo']);
+  }
 }
