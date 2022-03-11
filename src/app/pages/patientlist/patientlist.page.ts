37c3f04850086f983fe47b67fcae702557dcffc1
Ashish Musani <ashishmusani@Ashishs-MacBook-Air.local>
2022-03-10 20:16:40 -0500

    Added http dependency. Added web service connection patients' list page

--- src/app/pages/patientlist/patientlist.page.ts
+++ src/app/pages/patientlist/patientlist.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-patientlist',
@@ -8,9 +9,19 @@ import { Router } from '@angular/router';
 })
 export class PatientlistPage implements OnInit {
 
-  constructor(private router: Router) { }
+  private patients: any = [];
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('http://localhost:5000/patients').subscribe(data => {
+      console.log(data);
+      if(data){
+        this.patients = data
+      }
+    }, err => {
+      console.log(err)
+    })
   }
 
   navigateToPatientInfo() {
